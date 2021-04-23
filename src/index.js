a2b55e9320c291e9886854a6caf8f702ea7cf79b
Payam Abdullah <payam97abubakr@gmail.com>
2021-04-23 20:55:32 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  const tasks = document.getElementById('new-task-description');
+  const inputtasks = document.getElementById('new-task-description');
   const form = document.getElementById('create-task-form');
   const list = document.getElementById('list');
   const task = document.getElementById('tasks')
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    let inputTextValue = tasks.value;
+    let inputTextValue = inputtasks.value;
     let li = document.createElement('li');
     li.innerText = inputTextValue;
     taskUL.append(li);
