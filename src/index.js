a876ee7418c09c5c1e6b3f13cd7232caa157ac05
Payam Abdullah <payam97abubakr@gmail.com>
2021-04-23 20:56:43 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const inputtasks = document.getElementById('new-task-description');
+  const form = document.getElementById('create-task-form');
+  const list = document.getElementById('list');
+  const task = document.getElementById('tasks')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let inputText = inputtasks.value;
+    let li = document.createElement('li');
+    li.innerText = inputText;
+    task.append(li);
+  });
 });
