637bb26ebadaca355de04afed7240999b44eaca5
Payam Abdullah <payam97abubakr@gmail.com>
2021-04-23 20:51:55 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let d = new Date();
+  document.getElementById("date").innerHTML = d.getFullYear()+"/"+(d.getMonth() + 1)+"/"+d.getDate();
+  
+  //creating a list after clicking on add
+  function newElement() {
+      let li = document.createElement("li");
+      let inputValue = document.getElementById("txtinput").value;
+      let txtnode = document.createTextNode(inputValue);
+      li.appendChild(txtnode);
+        document.getElementById("todolist").appendChild(li);
+        document.getElementById("txtinput").value = "";
+    } 
 });
